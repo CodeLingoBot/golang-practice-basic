3b2758212ed0a70ab88891aa31e4f5e84a08d5e7
HiepPP <zendragon93@gmail.com>
2018-12-27 23:31:52 +0700

    update

--- type/type3.go
+++ type/type3.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 //var x int
+var z *int
 func main(){
 	x := 1993
 	p := &x
-	fmt.Println(x)
-	fmt.Println(p)
-	fmt.Println(&p)
-	fmt.Println(&x)
-	fmt.Println(*p)
+	fmt.Println(x) // 1993
+	fmt.Println(p) // 0xc000014050
+	fmt.Println(&p) // 0xc00000c028
+	fmt.Println(&x) // 0xc000014050
+	fmt.Println(*p) // 1993
 	//fmt.Println(*x) invalid indirect of x (type int)
-	fmt.Println(p == &x)
-	fmt.Println(*p == x)
+	fmt.Println(p == &x) //true
+	fmt.Println(*p == x) // true
+	//z = &p cannot use &p (type **int) as type *int in assignment
+	z = &x
+	fmt.Println(z)
 }

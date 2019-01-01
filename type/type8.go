57c12bdc1c7060f5b67dee0f7745ecdcf55ddac1
HiepPP <zendragon93@gmail.com>
2019-01-02 00:51:56 +0700

    update

--- type/type8.go
+++ type/type8.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type PInterface interface{
+	TestParent1(i int)
+}
+
 type Parent struct {
 	name string
 }
@@ -19,10 +23,21 @@ func TestParent(p Parent){
 	fmt.Println(p.name)
 }
 
+func (p Parent) TestParent1(i int){
+	fmt.Println(i)
+}
+
+func testInterface(pInterface PInterface){
+
+}
+
 func main(){
-	//p := Parent{name: "parent"}
+	p := Parent{name: "parent"}
 	//Test(p) //cannot use p (type Parent) as type Child in argument to Test
 	c :=Child{age:18, Parent: Parent{name:"parent"}}
 	TestChild(c)
 	TestParent(c.Parent)
+
+	testInterface(c)
+	testInterface(p)
 }

7eac90fb88b6cc596ceca4904a220143fe13bdd0
HiepPP <zendragon93@gmail.com>
2019-01-05 23:09:27 +0700

    update

---
+++ crawler/colly1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector()
+
+
+	c.OnHTML("li.active > ul> li> a", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+		fmt.Printf("%s%s", "https://comicvn.net", e.Attr("href"))
+		fmt.Println(e.Attr("---------"))
+	})
+
+
+	c.Visit("https://comicvn.net/truyen-tranh-online/iron-wok-jan-dau-bep-sieu-dang-13283")
+}
---
+++ crawler/colly2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector()
+
+
+	c.OnHTML("div.manga-chapter-image", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+		fmt.Println("---------")
+	})
+
+
+	c.Visit("https://comicvn.net/truyen-tranh-online/iron-wok-jan-dau-bep-sieu-dang/chapter-1-316596")
+}
